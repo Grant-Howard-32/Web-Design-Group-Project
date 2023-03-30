cb00eebd328c1a110cc84a99046efcddb439f18e
ao002 <ao002@marietta.edu>
2023-03-30 19:57:35 -0400

    update Nav bar to php

--- registerCourse.php
+++ registerCourse.php
@@ -6,48 +6,7 @@
     <script src="script.js"></script>
   </head>
   <body>
-    <nav>
-      <div class="logo">
-        <a href="#">
-          <img src="Icons/aperture.svg">
-        </a>
-      </div>
-      <div class="menu">
-        <a href="home.html">
-          <img src="Icons/home.svg" title="Home">
-        </a>
-        <div class="dropdown">
-          <img src="Icons/user-plus.svg" title="Add Instructor / Enroll Student">
-          <div class="dropdown-content">
-            <a href="addInstructor.html">Add Instructor</a>
-            <a href="enrollStudent.html">Enroll Student</a>
-          </div>
-        </div>
-        <div class="dropdown">
-          <img src="Icons/plus-circle.svg" title="Modify Courses">
-          <div class="dropdown-content">
-            <a href="addCourse.html">Add Course</a>
-            <a href="registerCourse.html">Register Course</a>
-            <a href="dropCourse.html">Drop Course</a>
-          </div>
-        </div>
-        <div class="dropdown">
-          <img src="Icons/calendar.svg" title="Calendar">
-          <div class="dropdown-content">
-            <a href="selectStudent.html">Courses Registered</a>
-            <a href="selectCourse.html">Students Registered to Course</a>
-            <a href="selectInstructor.html">Courses Taught</a>
-            <a href="selectSemester.html">Students Registered by Semester</a>
-          </div>
-        </div>
-        <a href="usermanualv4.html">
-          <img src="Icons/paperclip.svg" title="User Manual">
-        </a>
-        <a href="accountInfo.html">
-          <img src="Icons/user.svg" title="Programmer's Manual">
-        </a>
-      </div>
-    </nav>
+    <?php include 'navigation.php'; ?>
     <h1 id="title">Register Course</h1>
     <div class="form_container">
       <form onsubmit="return validateRegisterDropCourseForm();" action="dropCourse.php" method="post">
